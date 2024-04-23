0e1162a5ee070bf42c5f88a4455c9a6d6b6295c1
pikimaru <pikajunglist@gmail.com>
2024-04-23 11:08:06 +0200

    fixed origins

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 const connectDB = require('./dbConn')
 const express = require('express');
 const app = express()
 // const path = require('path');
-const PORT = process.env.PORT || 3500;
+// const PORT = process.env.PORT || 3500;
 
 // const WebSocket = require('ws')
 
@@ -20,13 +21,15 @@ const Users = require('./usersModel')
 connectDB()
 
 app.use(express.json())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
 
-    next();
-  });
+//     next();
+//   });
+
+app.use(cors())
 
 
 app.get('/',(req,res)=>{
@@ -52,7 +55,7 @@ app.patch('/updateUserSellNBuy',updateUserSellNBuy)
 
 app.patch('/resetUser',resetUser)
 
-app.post('/deleteStock',deleteStock)
+app.patch('/deleteStock',deleteStock)
 
 
 
@@ -64,4 +67,4 @@ mongoose.connection.once('open',()=>{
 
 
 
-app.listen(PORT,()=> console.log(`Server running on ${PORT}`))
+app.listen(3500,()=> console.log(`Server running on 3500`))
